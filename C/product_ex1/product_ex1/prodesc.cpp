4ca924ea1fc76667359ea2643e6b86af9c607dd6
joaotrindade <joao.ferreira.trindade@gmail.com>
2015-04-03 13:37:48 +0100

    [C++] Adicionadas funçoes para OpenMP

--- C/product_ex1/product_ex1/prodesc.cpp
+++ C/product_ex1/product_ex1/prodesc.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include "myClass.h"
 #define MIN(a,b) a<b ? a:b
+#pragma comment(linker, "/STACK:3246391296") // ~3GB | Precisamos de mais do que 2GB para fazer 10000 x 10000
 
 using namespace std;
 
@@ -108,15 +109,15 @@ void OnMult(int m_ar, int m_br, int nt)
 
 
 		GetLocalTime(&Time2);
-		sprintf(st, "Time: %3.3f seconds\n", ((float) DifTime(Time1, Time2)/1000.0));
-		cout << st;
+		std::sprintf(st, "Time: %3.3f seconds\n", ((float) DifTime(Time1, Time2)/1000.0));
+		std::cout << st;
 
-		cout << "Result matrix: " << endl;
+		std::cout << "Result matrix: " << endl;
 		for(i=0; i<1; i++)
 		{	for(j=0; j<min(10,m_br); j++)
-				cout << C(i,j) << " ";
+				std::cout << C(i,j) << " ";
 		}
-		cout << endl;
+		std::cout << endl;
 
 		GlobalUnlock(ha);
 		GlobalUnlock(hb);
@@ -183,16 +184,16 @@ void OnMultOptimized(int m_ar, int m_br, int nt)
 
 
 		GetLocalTime(&Time2);
-		sprintf(st, "Time: %3.3f seconds\n", ((float)DifTime(Time1, Time2) / 1000.0));
-		cout << st;
+		std::sprintf(st, "Time: %3.3f seconds\n", ((float)DifTime(Time1, Time2) / 1000.0));
+		std::cout << st;
 
-		cout << "Result matrix: " << endl;
+		std::cout << "Result matrix: " << endl;
 		for (i = 0; i<1; i++)
 		{
 			for (j = 0; j<min(10, m_br); j++)
-				cout << C(i, j) << " ";
+				std::cout << C(i, j) << " ";
 		}
-		cout << endl;
+		std::cout << endl;
 
 		GlobalUnlock(ha);
 		GlobalUnlock(hb);
@@ -278,15 +279,15 @@ void OnMultblock(int m_ar, int m_br, int nt)
 
 
 		GetLocalTime(&Time2);
-		sprintf(st, "Time: %3.3f seconds\n", ((float) DifTime(Time1, Time2)/1000.0));
-		cout << st;
+		std::sprintf(st, "Time: %3.3f seconds\n", ((float) DifTime(Time1, Time2)/1000.0));
+		std::cout << st;
 
-		cout << "Result matrix: " << endl;
+		std::cout << "Result matrix: " << endl;
 		for(i=0; i<1; i++)
 		{	for(j=0; j<min(10,m_br); j++)
-				cout << C(i,j) << " ";
+				std::cout << C(i,j) << " ";
 		}
-		cout << endl;
+		std::cout << endl;
 
 		GlobalUnlock(ha);
 		GlobalUnlock(hb);
@@ -312,8 +313,177 @@ float produtoInterno(float *v1, float *v2, int col)
 
 }
 
+//TODO: THIS
+void OnMultParallel(int m_ar, int m_br, int nt) {
+	// TODO: Add your command handler code here
+	SYSTEMTIME Time1, Time2;
+	HGLOBAL ha, hb, hc;
+	double *pha, *phb, *phc;
+	char st[100];
+	double temp;
+	int i, j, k;
+	int n_thread = 1;
+
+	std::cout << "Insert number of threads: " << endl;
+	cin >> n_thread;
 
 
+	if (1)
+	{
+		ha = GlobalAlloc(GMEM_FIXED, (m_ar * m_ar) * sizeof(double));
+		hb = GlobalAlloc(GMEM_FIXED, m_br * m_br * sizeof(double));
+		hc = GlobalAlloc(GMEM_FIXED, m_ar * m_br * sizeof(double));
+
+		pha = (double *)GlobalLock(ha);
+		phb = (double *)GlobalLock(hb);
+		phc = (double *)GlobalLock(hc);
+
+		CdMatriz A(pha, m_ar, m_ar);
+		CdMatriz B(phb, m_br, m_br);
+		CdMatriz C(phc, m_ar, m_br);
+
+
+		for (i = 0; i < m_ar; i++)
+			for (j = 0; j < m_ar; j++)
+				A(i, j, (double)1.0);
+
+
+		for (i = 0; i < m_br; i++)
+			for (j = 0; j < m_br; j++)
+				B(i, j, (double)(i + 1));
+
+		GetLocalTime(&Time1);
+
+		#pragma omp parallel for num_threads(n_thread) private(i,j)
+		for (i = 0; i < m_ar; i++)
+		{
+			for (j = 0; j < m_br; j++)
+			{
+				temp = 0;
+				for (k = 0; k < m_ar; k++)
+				{
+					temp += A(i, k) * B(k, j);
+				}
+				C(i, j, temp);
+			}
+		}
+
+		/*for(i=0; i<m_ar; i++)
+		{	for( k=0; k<m_br; k++)
+		{
+		for( j=0; j<m_ar; j++)
+		{
+		C(i, j, C(i, j) + A(i, k) * B(k, j));
+		}
+
+		}
+		}*/
+
+
+		GetLocalTime(&Time2);
+		std::sprintf(st, "Time: %3.3f seconds\n", ((float)DifTime(Time1, Time2) / 1000.0));
+		std::cout << st;
+
+		std::cout << "Result matrix: " << endl;
+		for (i = 0; i < 1; i++)
+		{
+			for (j = 0; j < min(10, m_br); j++)
+				std::cout << C(i, j) << " ";
+		}
+		std::cout << endl;
+
+		GlobalUnlock(ha);
+		GlobalUnlock(hb);
+		GlobalUnlock(hc);
+
+		::GlobalFree(ha);
+		::GlobalFree(hb);
+		::GlobalFree(hc);
+
+
+	}
+}
+
+//TODO: THIS
+void OnMultOptimizedParallel(int m_ar, int m_br, int nt)
+{
+	// TODO: Add your command handler code here
+	SYSTEMTIME Time1, Time2;
+	HGLOBAL ha, hb, hc;
+	double *pha, *phb, *phc;
+	char st[100];
+	double temp;
+	int i, j, k;
+	int n_thread = 1;
+
+	std::cout << "Insert number of threads: " << endl;
+	cin >> n_thread;
+
+
+	if (1)
+	{
+		ha = GlobalAlloc(GMEM_FIXED, (m_ar * m_ar) * sizeof(double));
+		hb = GlobalAlloc(GMEM_FIXED, m_br * m_br * sizeof(double));
+		hc = GlobalAlloc(GMEM_FIXED, m_ar * m_br * sizeof(double));
+
+		pha = (double *)GlobalLock(ha);
+		phb = (double *)GlobalLock(hb);
+		phc = (double *)GlobalLock(hc);
+
+		CdMatriz A(pha, m_ar, m_ar);
+		CdMatriz B(phb, m_br, m_br);
+		CdMatriz C(phc, m_ar, m_br);
+
+
+		for (i = 0; i<m_ar; i++)
+			for (j = 0; j<m_ar; j++)
+				A(i, j, (double)1.0);
+
+
+		for (i = 0; i<m_br; i++)
+			for (j = 0; j<m_br; j++)
+				B(i, j, (double)(i + 1));
+
+		GetLocalTime(&Time1);
+
+		#pragma omp parallel for num_threads(n_thread) private(i,k)
+		for (i = 0; i<m_ar; i++)
+		{
+			for (k = 0; k<m_br; k++)
+			{
+				for (j = 0; j<m_ar; j++)
+				{
+					C(i, j, C(i, j) + A(i, k) * B(k, j));
+				}
+
+			}
+		}
+
+
+		GetLocalTime(&Time2);
+		std::sprintf(st, "Time: %3.3f seconds\n", ((float)DifTime(Time1, Time2) / 1000.0));
+		std::cout << st;
+
+		std::cout << "Result matrix: " << endl;
+		for (i = 0; i<1; i++)
+		{
+			for (j = 0; j<min(10, m_br); j++)
+				std::cout << C(i, j) << " ";
+		}
+		std::cout << endl;
+
+		GlobalUnlock(ha);
+		GlobalUnlock(hb);
+		GlobalUnlock(hc);
+
+		::GlobalFree(ha);
+		::GlobalFree(hb);
+		::GlobalFree(hc);
+
+
+	}
+
+}
 
 int main (int argc, char *argv[])
 {
@@ -328,9 +498,11 @@ int main (int argc, char *argv[])
 	
 	op=1;
 	do {
-		cout << endl << "1. Multiplication" << endl;
-		cout << "2. Optimized Multiplication" << endl;
-		cout << "Selection?: ";
+		std::cout << endl << "1. Multiplication" << endl;
+		std::cout << "2. Optimized Multiplication" << endl;
+		std::cout << "3. Multiplication (Parallel)" << endl;
+		std::cout << "4. Optimized Multiplication (Parallel)" << endl;
+		std::cout << "Selection?: ";
 		scanf_s("%d", &op);
 		if (op == 0)
 			break;
@@ -345,10 +517,16 @@ int main (int argc, char *argv[])
 			case 2:
 				OnMultOptimized(lin, col, nt);
 				break;
+			case 3:
+				OnMultParallel(lin, col, nt);
+				break;
+			case 4:
+				OnMultOptimizedParallel(lin, col, nt);
+				break;
 		}
 	}while (op != 0);
 
-	cout << "Enter to continue ...";
+	std::cout << "Enter to continue ...";
 	cin.get(c);
 	cin.get(c);
 	
