1bcfb0508aef7c603165de56aee61b1844a55b44
joaotrindade <joao.ferreira.trindade@gmail.com>
2015-05-16 17:21:57 +0100

    openMP terminado

--- proj2/openmp/crivo.cpp
+++ proj2/openmp/crivo.cpp
@@ -11,6 +11,42 @@ using namespace std;
 #define USED_CLOCK CLOCK_MONOTONIC // CLOCK_MONOTONIC_RAW if available
 #define NANOS 1000000000LL
 
+bool* isprime ;
+
+void computeChunk(unsigned long seeds[], int number_seeds, int startIndex, int endIndex, int CHUNKSIZE)
+{
+    int maxLookupIndex = ceil(sqrt(endIndex)) + startIndex ;
+    //cout << " computeChunk start: " << startIndex << " maxLookupIndex: " << maxLookupIndex << " chunk end: " << endIndex <<  endl; 
+    bool foundFirst ; 
+    
+    for(int l = 0 ; l < number_seeds ; l++ )
+    {
+	int temp_seed = seeds[l];
+	if (temp_seed  > maxLookupIndex ) break ; 
+	foundFirst = false;
+	for(int i = startIndex ; i <= maxLookupIndex; i++)
+	{
+	    if (foundFirst) break; 
+	    if (i % temp_seed == 0) // encontrou um para marcar
+	    {
+		//cout << "a semenete : " << temp_seed << " marcou o numero" << i << endl;
+		isprime[i] = false; 
+		foundFirst = true;
+		for(int j = i ; j <= endIndex; j+= temp_seed)
+		{
+		    //cout << "---- a semenete : " << temp_seed << " marcou o numero" << j << endl;
+		    isprime[j] = false;
+		}
+	    }
+	}
+    }
+    
+    
+    //int stop ; cin >> stop ; 
+
+  
+}
+
 
 int main()
 {
@@ -18,7 +54,7 @@ int main()
     unsigned long i;
     struct timespec begin, current;
     long long start, elapsed, microseconds;
-    bool* isprime ;
+    
     unsigned long pCount = 0;
     unsigned long* seeds;
     int CHUNKSIZE = 0;
@@ -31,7 +67,7 @@ int main()
     
     isprime = new bool[n];
     seeds = new unsigned long[(int)sqrt(n) + 5] ;
-    CHUNKSIZE = (int) ( (n - (int)sqrt(n) ) / NUM_THREADS) + 1 ;
+    CHUNKSIZE = ceil( ( (n - (int)sqrt(n) ) / NUM_THREADS) + 1 );
     cout << "chunksize : " << CHUNKSIZE << endl;
     memset(isprime, true, n);
     isprime[0] = isprime[1] = false;
@@ -39,12 +75,12 @@ int main()
     if (clock_gettime(USED_CLOCK, &begin)) exit(EXIT_FAILURE);
     start = begin.tv_sec*NANOS + begin.tv_nsec;
 
-    for(i=2 ; i<sqrt(n)+1; i++)
+    for(i=2 ; i<=sqrt(n)+1; i++)
     {
 	if(isprime[i])
 	{
 
-	  for(int j = i*i ; j < sqrt(n); j+=i)
+	  for(int j = i*i ; j <= sqrt(n); j+=i)
 	  {
 	    isprime[j] = false;
 	  }
@@ -52,57 +88,41 @@ int main()
 	}
     }
     
-    cout << "seeds: ";
+    //cout << "seeds: ";
     for (int i = 2 ; i < sqrt(n) ; i++)
-      if (isprime[i] == true) { seeds[number_seeds++] = i ; cout << i << " ";}
+      if (isprime[i] == true) { seeds[number_seeds++] = i ; /*cout << i << " ";*/}
     cout << endl;
     
     number_seeds = number_seeds ;
-    int* chunk_max_index = new int[NUM_THREADS] ;
-    int* chunk_min_index = new int[NUM_THREADS] ;
+    //cout << "got " << number_seeds << " seeds" << endl;
+
+    int* nextStartIndex = new int[NUM_THREADS + 1 ];
+    int* nextEndIndex = new int[NUM_THREADS + 1]; 
     
-    cout << "got " << number_seeds << " seeds" << endl;
-    
-    for(int i = 0; i < NUM_THREADS; i++) chunk_max_index[i] = 0 ;
-    #pragma omp parallel num_threads(4)
+    nextStartIndex[1]  = ceil(sqrt(n));
+    nextEndIndex[1] = nextStartIndex[1] + CHUNKSIZE ;
+    //cout << "i: 1" << " start : " << nextStartIndex[1] << " -  end: " << nextEndIndex[1] << endl;
+    for(int i = 2; i<=NUM_THREADS; i++)
     {
-	  #pragma omp for schedule(static,CHUNKSIZE) private(i)
-	  for(i=sqrt(n) ; i<n ; i++)
+	nextStartIndex[i] = nextEndIndex[i - 1];
+	nextEndIndex[i] = nextStartIndex[i] + CHUNKSIZE ;
+	//cout << "i: " << i << " start : " << nextStartIndex[i] << " -  end: " << nextEndIndex[i] << endl;
+    }
+	   
+    #pragma omp parallel num_threads(NUM_THREADS)
+    {
+    
+	  #pragma omp single nowait
 	  {
-		if (i <= sqrt(chunk_min_index[omp_get_thread_num()]))
-		{
-		    // UPDATE MIN AND MAX INDEXES OF CHUNK
-		    if (chunk_max_index[omp_get_thread_num()] == 0)
-		    {
-			chunk_min_index[omp_get_thread_num()] = i ;
-			if (i + CHUNKSIZE < n) chunk_max_index[omp_get_thread_num()] = i + CHUNKSIZE;
-			else chunk_max_index[omp_get_thread_num()] = n ;
-		    }
-		    
-		    
-		    
-		    //#pragma omp critical
-		    //cout << "Thread : " << omp_get_thread_num() << " index : " << i << " max_index : " << chunk_max_index[omp_get_thread_num()] << endl;
-		    
-		    //PARSE CHUNK
-		    if (isprime[i])
-		    {
-			for(int l = 0 ; l < number_seeds ; l++ )
-			{
-			    int temp_seed = seeds[l];
-			    if (temp_seed > i) break;
-			    if (i % temp_seed == 0) // encontrou um para marcar
-			    {
-				for(int j = i*i ; j < chunk_max_index[omp_get_thread_num()]; j+=temp_seed)
-				{
-				  isprime[j] = false;
-				}
-			    }
-			}
-		    }
-		}
+	      for(int i = 1; i<=NUM_THREADS; i++)
+	      {
+		    //#pragma omp task
+		    computeChunk(seeds, number_seeds, nextStartIndex[i], nextEndIndex[i], CHUNKSIZE);
+	      }
+	      
 	  }
     }
+	  
     
     
     if (clock_gettime(USED_CLOCK, &current)) exit(EXIT_FAILURE);
